9cf9dc05da0d1935e155db05c3739acd84a4fc4f
Abhishek Chetani <chetaniabhishek@outlook.com>
2025-03-29 16:40:26 +0530

    Refactoring & Minor Fixes

--- app.js
+++ app.js
@@ -1,82 +1,106 @@
-const form = document.querySelector('#searchForm');
+const form = document.querySelector("#searchForm");
+
+const sendRequest = async query => {
+	try {
+		// Storing Query Parameter from Input in Separate Variable
+		const config = { params: { q: query } };
+
+		// API Request using Axios with 'CONFIG'
+		const res = await axios.get("http://api.tvmaze.com/search/shows", config);
+
+		// Passing Response Array to the Function
+		displayShows(res.data);
+	} catch (e) {
+		console.log("NO RESPONSE FROM SERVER - ", e);
+	}
+};
 
 // Checking for form submit event
-form.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    // Checking 'form' directory for input value
-    console.dir(form);
-    // Capturing Form Input Value By User
-    const searchTerm = form.elements.query.value;
+form.addEventListener("submit", e => {
+	e.preventDefault();
 
-    // Requesting from API with String Template Literal
-    // const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
-    // console.log(res.data);
-    // Getting The Image URL Of First Show From Response
-    // console.log(res.data[0].show.image.medium);
+	// Checking 'form' directory for input value
+	// console.dir(form);
 
-    try {
-        // Setting Query Parameter from Input in an Object
-        const config = { params: { q: searchTerm } };
-        // API Request using Axios with Object
-        const res = await axios.get("http://api.tvmaze.com/search/shows", config);
-        // Passing Response Data to the Function
-        makeImages(res.data);
-        // Resetting Search Input after every Search
-        form.elements.query.value = "";
-    } catch (e) {
-        // Handling Error in case of No Response
-        console.log("NO RESPONSE FROM SERVER - ", e);
-    }
-})
+	// Capturing Form Input Value By User
+	const searchTerm = form.elements.query.value;
+	sendRequest(searchTerm);
+
+	// Resetting Search Input after every Search
+	form.elements.query.value = "";
+	// Removing Previous Search Results
+	const prevResults = document.querySelectorAll(".show");
+	removePrevResults(prevResults);
+
+	// try {
+	// 	// Requesting from API with String Template Literal
+	// 	const res = await axios.get(
+	// 		`http://api.tvmaze.com/search/shows?q=${searchTerm}`
+	// 	);
+	// 	console.log(res.data); // Returns an array of objects
+
+	// 	// Getting The Image URL Of First Show From Response
+	// 	console.log(res.data[0].show.image.medium);
+
+	// } catch (e) {
+	// 	// Handling Error in case of No Response
+	// 	console.log("NO RESPONSE FROM SERVER - ", e);
+	// }
+});
 
 // Selecting the Container
-const imageContainer = document.querySelector('#container');
+const imageContainer = document.querySelector("#container");
 // Function to Display Search Results
-const makeImages = (shows) => {
-    try {
-        for (let result of shows) {
-            console.log(result);            // checking the received response in function call
+const displayShows = shows => {
+	try {
+		for (let result of shows) {
+			// If Enough Data Exists For A Show, Then Append It To Webpage
+			if (result.show.image && result.show.network) {
+				const show = document.createElement("div");
+				// Adding .show class to newly created 'div'
+				show.classList.add("show");
 
-            // If Image Exists For A Show, Then Append It To Webpage
-            if (result.show.image && result.show.name && result.show.webChannel && result.show.officialSite) {
-                const show = document.createElement('div');
-                // Adding .show class to newly created 'div'
-                show.classList.add('show');
+				const showLink = document.createElement("a");
+				const showImg = document.createElement("img");
 
-                const img = document.createElement('img');
-                // Setting Image Source & It's Link To Official Site
-                img.src = result.show.image.medium;
-                img.setAttribute('href', result.show.officialSite);
-                // Adding .image class to every 'img'
-                img.classList.add('image');
-                // Appending 'img' to the 'div' element
-                show.appendChild(img);
+				// Setting Image Source
+				showImg.src = result.show.image.medium;
+				// Adding .image class to every 'img'
+				showImg.classList.add("image");
 
-                // Displaying the Show Name under the Image
-                const showName = document.createElement('h3');
-                showName.innerText = result.show.name;
-                show.appendChild(showName);
+				// Setting Image Link to Official Site
+				showLink.href = result.show.officialSite;
+				// Setting Image Target to Open In New Tab
+				showLink.target = "_blank";
 
-                // Available Source/Channel for every Result
-                const channel = document.createElement('span');
-                channel.append(` - ${result.show.webChannel.name}`);
-                // Inserting 'span' after 'h3' element
-                showName.insertAdjacentElement('afterend', channel);
+				// Appending 'img' to 'a' & 'a' to 'div'
+				showLink.appendChild(showImg);
+				show.appendChild(showLink);
 
-                // Appending everything to the Main Container
-                imageContainer.appendChild(show);
+				// Displaying the Show Name under the Image
+				const showName = document.createElement("h3");
+				showName.innerText = result.show.name;
+				show.appendChild(showName);
 
-                // removeImages(img);
-            }
-        }
-    } catch (e) {
-        console.log("DATA NOT AVAILABLE - ", e);
-    }
-}
+				// Available Source/Channel for every Result
+				const network = document.createElement("span");
+				network.append(` - ${result.show.network.name}`);
+				// Inserting 'span' after 'h3' element
+				showName.insertAdjacentElement("afterend", network);
 
-// Function For Removing Show Images On Next Show Search
-const removeImages = (images) => {
-    for (let image of images) {
-        image.parentElement.removeChild(image);
-    }
-}
+				// Appending everything to the Main Container
+				imageContainer.appendChild(show);
+			}
+		}
+	} catch (e) {
+		console.log("DATA NOT AVAILABLE - ", e);
+	}
+};
+
+// Removing Show Images On Next Show Search
+const removePrevResults = shows => {
+	for (let result of shows) {
+		// result.parentElement.removeChild(result);
+		result.remove();
+	}
+};
